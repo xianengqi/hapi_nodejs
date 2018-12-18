8c55e7599186140e524f1e02178db4f678a82435
xianengqi <920986617@qq.com>
2018-12-18 14:55:09 +0800

    获取小程序JWT

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ const Joi = require('joi');
 const JWT = require('jsonwebtoken');
 const axios = require('axios');
 const config = require('../config');
+const models = require('../models');
+const decryptData = require('../utils/decrypted-data');
+
 const GROUP_NAME = 'users';
+
 module.exports = [
   {
     method: 'POST',
@@ -34,7 +38,7 @@ module.exports = [
       const appid = config.wxAppid; // 你的小程序 appid
       const secret = config.wxSecret; // 你的小程序 appsecret
       const { code, encryptedData, iv } = req.payload;
-
+      // 向微信小程序开放平台 换取openid 与 session_key
       const response = await axios({
         url: 'https://api.weixin.qq.com/sns/jscode2session',
         method: 'GET',
@@ -46,12 +50,40 @@ module.exports = [
         }
       });
       // response 中返回 openid 与 session_key
-      const { openid, session_key } = response.data;
-      reply();
+      const { openid, session_key: sessionKey } = response.data;
+      // 基于 openid 查找或创建一个用户
+      const user = await models.users.findOrCreate({
+        where: { open_id: openid },
+      });
+      // decrypt 解码用户信息
+      const userInfo = decryptData(encryptedData, iv, sessionKey, appid);
+      // 更新 user 表中的用户的资料信息
+      await models.users.update({
+        nick_name: userInfo.nickName,
+        gender: userInfo.gender,
+        avatar_url: userInfo.avatarUrl,
+        open_id: openid,
+        session_key: sessionKey,
+      },{
+        where: { open_id: openid },
+      });
+      
+      // 签发 jwt
+      const generateJWT = (jwtInfo) => {
+        const payload = {
+          userId: jwtInfo.userId,
+          exp: Math.floor(new Date().getTime() / 1000) + 7 * 24 * 60 * 60,
+        };
+        return JWT.sign(payload, config.jwtSecret);
+      };
+      reply(generateJWT({
+        userId: user[0].id,
+      }));
     },
     config: {
       auth: false, // 不需要用户验证
       tags: ['api', GROUP_NAME], // 注册 swagger 文档
+      description: '通过 openid 换取本地数据库的 user_id，签发 JWT',
       validate: {
         payload: {
           code: Joi.string().required().description('微信用户登录的临时code'),
